3e57ad013de1bb8ee158b7e1839e71aaa9b27450
Joshua Liddell <joshua.liddell21@gmail.com>
2025-09-04 19:03:35 -0600

    new subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,31 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use std::net::TcpListener;
 
 // This is the request handler function that returns something that implements the responder trait (can be
-// converted to http response)
+// converted to http response) (this could also be impl Responder no difference)
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run();
-
-    Ok(server)
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
 }
 
-// FIX CI pipeline before pushing
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
