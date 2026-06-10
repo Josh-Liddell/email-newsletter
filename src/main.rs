af86bd99f828810e3ebb18d927c01290aec7a7d5
Josh Liddell <joshua.liddell21@gmail.com>
2026-06-10 14:45:48 -0600

    initial setup steps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("hello world");
+use emailnewsletter::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
+
+// ultimately transfer this setup and etc over to the dashboard server
